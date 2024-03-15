8bc8aa2fe2fc70bb121fb43b31db7433e07a703e
anushaAcharya27 <143736122+anushaAcharya27@users.noreply.github.com>
2024-03-15 21:50:31 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,406 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+<a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/home-slide-1.jpg) no-repeat">
+            <div class="content">
+               <span>explore, discover, travel</span>
+               <h3>travel arround the world</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/home-slide-2.jpg) no-repeat">
+            <div class="content">
+               <span>explore, discover, travel</span>
+               <h3>discover the new places</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/home-slide-3.jpg) no-repeat">
+            <div class="content">
+               <span>explore, discover, travel</span>
+               <h3>make your tour worthwhile</h3>
+               <a href="package.php" class="btn">discover more</a>
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- services section starts  -->
+
+<section class="services">
+
+   <h1 class="heading-title"> our services </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/icon-1.png" alt="">
+         <h3>adventure</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-2.png" alt="">
+         <h3>tour guide</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-3.png" alt="">
+         <h3>trekking</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-4.png" alt="">
+         <h3>camp fire</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-5.png" alt="">
+         <h3>off road</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/icon-6.png" alt="">
+         <h3>camping</h3>
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- home about section starts  -->
+
+<section class="home-about">
+
+   <div class="image">
+      <img src="images/about-img.jpg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>about us</h3>
+      <p>Choose us for your travel needs and unlock a world of seamless experiences. With diverse destinations, intuitive search options, and round-the-clock support, your dream vacation is just a click away.</p>
+      <p>Whether you seek relaxation, adventure, or cultural immersion, let us be your trusted guide to unforgettable journeys.</p>
+
+      <a href="about.php" class="btn">read more</a>
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+<!-- home packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Delhi Tour Packages</h3>
+            <p>"Discover the rich heritage and vibrant culture of Delhi with our meticulously crafted tour packages. From exploring historical landmarks to indulging in delectable cuisine, our packages offer an immersive experience in the heart of India's capital."</p>
+            <h2>BDT 10,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Mumbai Tour Packages</h3>
+            <p>
+"Discover the eclectic charm of Mumbai with our specially crafted tour packages. From iconic landmarks to hidden gems, delve into the heart of India's bustling metropolis."</p>
+            <h2>BDT 9,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Manali Tour Packages</h3>
+            <p>
+"Escape to the picturesque beauty of Manali with our all-inclusive tour package. Explore snow-capped peaks, serene valleys, and indulge in thrilling adventures for an unforgettable getaway."</p>
+            <h2>BDT 11,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Japan Tour Packages</h3>
+            <p>
+            "Embark on a journey through the Land of the Rising Sun with our Japan tour packages. Immerse yourself in ancient traditions and modern marvels for an enriching experience."</p>            <h2>BDT 13,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>china Tour Packages</h3>
+            <p>
+
+            "Embark on an adventure through China with our expertly curated tour packages. Discover iconic landmarks and immerse yourself in the vibrant culture of this fascinating destination."</p>
+            <h2>BDT 7,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>singapur Tour Packages</h3>
+            <p>"Experience the allure of Singapore with our thoughtfully designed tour packages. From stunning skyline views to cultural treasures, explore the best of this dynamic city-state."</p>
+            <h2>BDT 12,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-7.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>spain Tour Packages</h3>
+            <p>
+"Indulge in the charm of Spain with our meticulously crafted tour packages. From historic landmarks to vibrant culture, immerse yourself in the allure of this captivating destination."</p>
+            <h2>BDT 18,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-8.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>canada Tour Packages</h3>
+            <p>"Embark on an adventure through Canada with our carefully curated tour packages. From breathtaking natural landscapes to vibrant cities, experience the diverse beauty of this enchanting country."</p>
+            <h2>BDT 21,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>barli Tour Packages</h3>
+            <p>
+"Discover the hidden gems of Barli with our exclusive tour packages. Immerse yourself in the serene beauty and cultural richness of this enchanting destination."</p>
+            <h2>BDT 14,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Nepal Tour Packages</h3>
+            <p>
+
+            "Experience the wonders of Nepal with our expertly tailored tour packages. From majestic Himalayan peaks to ancient temples, embark on an unforgettable journey through this breathtaking destination."</p>            <h2>BDT 7,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>bangladesh Tour Packages</h3>
+            <p>"Embark on a captivating journey through Bangladesh with our carefully curated tour packages. Explore vibrant cities, lush landscapes, and rich cultural heritage for an unforgettable adventure."</p>
+            <h2>BDT 5,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Goa Tour Packages</h3>
+            <p>
+"Immerse yourself in the vibrant spirit of Goa with our tailor-made tour packages. From sun-kissed beaches to lively nightlife, experience the best of this coastal paradise."</p>
+            <h2>BDT 11,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">see more</span></div>
+<button type="button" class="scroll-top"><i class="fa fa-angle-double-up" aria-hidden="true"></i></button>
+
+</section>
+
+<!-- home packages section ends -->
+
+<!-- home offer section starts  -->
+
+<section class="home-offer">
+   <div class="content">
+      <h3>upto 50% off</h3>
+      <p>"Exclusive Limited Time Offer:
+Book your dream vacation now and save up to 50% on select tours! Don't miss out on this incredible opportunity to explore breathtaking destinations at unbeatable prices. Start planning your adventure today!"</p>
+      <a href="book.php" class="btn">book now</a>
+   </div>
+</section>
+
+<!-- home offer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> Anu123@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i>Mukka, Manglore - 574146 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ package.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Travel Agency :: Best Agency</title>
+
+  <!-- swiper css link  -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+<!-- font awesome cdn link  -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+   <script>
+      $(document).ready(function(){
+          $(".scroll-top").click(function() {
+              $("html, body").animate({ 
+                  scrollTop: 0 
+              }, "slow");
+              return false;
+          });
+      });
+   </script>
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+<a href="home.php" class="logo">travel.</a>
+
+   <nav class="navbar">
+      <a href="home.php">home</a>
+      <a href="about.php">about</a>
+      <a href="package.php" class="active">package</a>
+      <a href="book.php">book</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<div class="heading" style="background:url(images/header-bg-2.png) no-repeat">
+   <h1>packages</h1>
+</div>
+
+<!-- packages section starts  -->
+
+<section class="packages">
+
+   <h1 class="heading-title">top destinations</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Delhi Tour Packages</h3>
+            <p>"Discover the rich heritage and vibrant culture of Delhi with our meticulously crafted tour packages. From exploring historical landmarks to indulging in delectable cuisine, our packages offer an immersive experience in the heart of India's capital."</p>
+            <h2>BDT 10,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-2.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Mumbai Tour Packages</h3>
+            <p>
+"Discover the eclectic charm of Mumbai with our specially crafted tour packages. From iconic landmarks to hidden gems, delve into the heart of India's bustling metropolis."</p>
+            <h2>BDT 9,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-3.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Manali Tour Packages</h3>
+            <p>
+"Escape to the picturesque beauty of Manali with our all-inclusive tour package. Explore snow-capped peaks, serene valleys, and indulge in thrilling adventures for an unforgettable getaway."</p>
+            <h2>BDT 11,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-4.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Japan Tour Packages</h3>
+            <p>
+            "Embark on a journey through the Land of the Rising Sun with our Japan tour packages. Immerse yourself in ancient traditions and modern marvels for an enriching experience."</p>            <h2>BDT 13,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-5.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>china Tour Packages</h3>
+            <p>
+
+            "Embark on an adventure through China with our expertly curated tour packages. Discover iconic landmarks and immerse yourself in the vibrant culture of this fascinating destination."</p>
+            <h2>BDT 7,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-6.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>singapur Tour Packages</h3>
+            <p>"Experience the allure of Singapore with our thoughtfully designed tour packages. From stunning skyline views to cultural treasures, explore the best of this dynamic city-state."</p>
+            <h2>BDT 12,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-7.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>spain Tour Packages</h3>
+            <p>
+"Indulge in the charm of Spain with our meticulously crafted tour packages. From historic landmarks to vibrant culture, immerse yourself in the allure of this captivating destination."</p>
+            <h2>BDT 18,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-8.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>canada Tour Packages</h3>
+            <p>"Embark on an adventure through Canada with our carefully curated tour packages. From breathtaking natural landscapes to vibrant cities, experience the diverse beauty of this enchanting country."</p>
+            <h2>BDT 21,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-9.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>barli Tour Packages</h3>
+            <p>
+"Discover the hidden gems of Barli with our exclusive tour packages. Immerse yourself in the serene beauty and cultural richness of this enchanting destination."</p>
+            <h2>BDT 14,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-10.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Nepal Tour Packages</h3>
+            <p>
+
+            "Experience the wonders of Nepal with our expertly tailored tour packages. From majestic Himalayan peaks to ancient temples, embark on an unforgettable journey through this breathtaking destination."</p>            <h2>BDT 7,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-11.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>bangladesh Tour Packages</h3>
+            <p>"Embark on a captivating journey through Bangladesh with our carefully curated tour packages. Explore vibrant cities, lush landscapes, and rich cultural heritage for an unforgettable adventure."</p>
+            <h2>BDT 5,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/img-12.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>Goa Tour Packages</h3>
+            <p>
+"Immerse yourself in the vibrant spirit of Goa with our tailor-made tour packages. From sun-kissed beaches to lively nightlife, experience the best of this coastal paradise."</p>
+            <h2>BDT 11,900</h2>
+            <a href="book.php" class="btn">book now</a>
+         </div>
+      </div>
+
+   </div>
+
+   <div class="load-more"><span class="btn">see more</span></div>
+<button type="button" class="scroll-top"><i class="fa fa-angle-double-up" aria-hidden="true"></i></button>
+
+</section>
+<!-- packages section ends -->
+<!-- footer section starts  -->
+
+<section class="footer">
+   <div class="box-container">
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="package.php"> <i class="fas fa-angle-right"></i> package</a>
+         <a href="book.php"> <i class="fas fa-angle-right"></i> book</a>
+      </div>
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+      </div>
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +880-1517-089144 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-2222-333333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> Anu123@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Mukka, Manglore - 574146  </a>
+      </div>
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+      </div>
+   </div>
+   
+</section>
+
+<!-- footer section ends -->
+<!-- swiper js link  -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
